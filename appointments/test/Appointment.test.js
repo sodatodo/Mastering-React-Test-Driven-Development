cfab5b3a23faaab10afb450b1a7ff2a5d0913d8f
soda <630281120@qq.com>
2019-10-25 20:38:09 +0800

    component without hard code

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -5,12 +5,23 @@ import { Appointment } from '../src/Appointment';
 describe('Appointment', () => {
     it('renders the customer first name', () => {
         const customer = { firstName: 'Ashley' };
-        const component = <Appointment customer={customer} />;
+        // const component = <Appointment customer={customer} />;
         const container = document.createElement('div');
 
-        document.body.appendChild(container);
-        ReactDOM.render(component, container);
+        // document.body.appendChild(container);
+        ReactDOM.render(<Appointment customer={customer}/>, container);
 
-        expect(document.body.textContent).toMatch('Ashley');
+        expect(container.textContent).toMatch('Ashley');
+    })
+
+    it('renders another customer first name', () => {
+        const customer = { firstName: 'Jordan' };
+        // const component = <Appointment customer={customer} />;
+        const container = document.createElement('div');
+
+        // document.body.appendChild(container);
+        ReactDOM.render(<Appointment customer={customer} />, container);
+
+        expect(container.textContent).toMatch('Jordan');
     })
 })
