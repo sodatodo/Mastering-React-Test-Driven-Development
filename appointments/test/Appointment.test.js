6135cd0337f1f4146a254e3c904baceb4af96b61
soda <stacksoda@163.com>
2019-10-29 17:52:47 +0800

    Adding events to a functional component

--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
@@ -15,7 +16,7 @@ describe('Appointment', () => {
         customer = { firstName: 'Ashley' };
 
         // ReactDOM.render(<Appointment customer={customer}/>, container);
-        render(<Appointment customer={customer}/>)
+        render(<Appointment customer={customer} />)
 
         expect(container.textContent).toMatch('Ashley');
     })
@@ -32,6 +33,17 @@ describe('Appointment', () => {
 
 describe('AppointmentsDayView', () => {
     let container;
+    const today = new Date();
+    const appointments = [
+        { 
+            startsAt: today.setHours(12, 0),
+            customer: { firstName: 'Ashley' }
+        },
+        { 
+            startsAt: today.setHours(13, 0),
+            customer: { firstName: 'Jordan' }
+        }
+    ];
     beforeEach(() => {
         container = document.createElement('div');
     });
@@ -47,22 +59,13 @@ describe('AppointmentsDayView', () => {
     })
     // 组件渲染了一个ol节点且有两个子节点
     it('renders multiple appointments in an ol element', () => {
-        const today = new Date();
-        const appointments = [
-            { startsAt: today.setHours(12, 0) },
-            { startsAt: today.setHours(13, 0) }
-        ];
         render(<AppointmentsDayView appointments={appointments} />);
         expect(container.querySelector('ol')).not.toBeNull();
         expect(container.querySelector('ol').children).toHaveLength(2);
     })
     // 组件渲染了两个li子节点 且内容随着参数而变化
     it('renders each appointment in an li', () => {
-        const today = new Date();
-        const appointments = [
-            { startsAt: today.setHours(12, 0) },
-            { startsAt: today.setHours(13, 0) }
-        ];
+        
         render(<AppointmentsDayView appointments={appointments} />);
 
         expect(container.querySelectorAll('li')).toHaveLength(2);
@@ -74,4 +77,35 @@ describe('AppointmentsDayView', () => {
             container.querySelectorAll('li')[1].textContent
         ).toEqual('13:00');
     })
+    // 包含指定字符串
+    it('initially shows a message saying there are no appointments today', () => {
+        render(<AppointmentsDayView appointments={[]} />);
+        expect(container.textContent).toMatch(
+            'There are no appointments scheduled for today.'
+        )
+    })
+
+    it('selects the first appointment by default', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.textContent).toMatch('Ashley');
+    })
+
+    it('has a button element in each li', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelectorAll('li > button')).toHaveLength(2);
+
+        expect(
+            container.querySelectorAll('li > button')[0].type
+        ).toEqual('button');
+    })
+
+    it('renders another appointment when selected', () => {
+        render(<AppointmentsDayView appointments={appointments} />);
+        const button = container.querySelectorAll('button')[1];
+        ReactTestUtils.Simulate.click(button);
+        expect(
+            container.textContent
+        ).toMatch('Jordan');
+    })
+
 })
