f05cf8a655c9468f7bbcb7f58873983a0916983b
soda <stacksoda@163.com>
2019-10-29 13:35:09 +0800

    render a list

--- appointments/src/Appointment.js
+++ appointments/src/Appointment.js
@@ -1,3 +1,13 @@
 import React from 'react';
 
 export const Appointment = ({ customer }) => (<div>{customer.firstName}</div>);
+
+export const AppointmentsDayView = ({ appointments }) => (
+  <div id="appointmentsDayView">
+    <ol>
+      {appointments.map(appointment => (
+        <div key={appointment.startsAt} />
+      ))}
+    </ol>
+  </div>
+  );
--- appointments/test/Appointment.test.js
+++ appointments/test/Appointment.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Appointment } from '../src/Appointment';
+import { Appointment, AppointmentsDayView } from '../src/Appointment';
 
 describe('Appointment', () => {
     let container;
@@ -27,5 +27,33 @@ describe('Appointment', () => {
         render(<Appointment customer={customer} />);
 
         expect(container.textContent).toMatch('Jordan');
+    })
+})
+
+describe('AppointmentsDayView', () => {
+    let container;
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+
+    const render = component =>
+        ReactDOM.render(component, container);
+
+    
+    it('renders a div with the right id', () => {
+        render(<AppointmentsDayView appointments={[]} />);
+
+        expect(container.querySelector('div#appointmentsDayView')).not.toBeNull();
+    })
+
+    it('renders multiple appointments in an ol element', () => {
+        const today = new Date();
+        const appointments = [
+            { startsAt: today.setHours(12, 0) },
+            { startsAt: today.setHours(13, 0) }
+        ];
+        render(<AppointmentsDayView appointments={appointments} />);
+        expect(container.querySelector('ol')).not.toBeNull();
+        expect(container.querySelector('ol').children).toHaveLength(2);
     })
 })
