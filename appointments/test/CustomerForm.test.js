c1592181da9c54ba1b32ee17a6674c976d436496
soda <stacksoda@163.com>
2019-10-31 09:19:34 +0800

    Extracting a form-finder method

--- appointments/test/CustomerForm.test.js
+++ appointments/test/CustomerForm.test.js
@@ -4,15 +4,17 @@ import { CustomerForm } from '../src/CustomerForm';
 
 describe('CustomerForm', () => {
   let render, container;
+  // 查找id为指定id的form标签
+  const form = id => container.querySelector(`form[id="${id}"]`);
 
   beforeEach(() => {
     ({ render, container } = createContainer());
   })
-
+  
   it('renders a form', () => {
     render(<CustomerForm />);
     expect(
-      container.querySelector('form[id="customer"]')
+      form('customer')
     ).not.toBeNull();
   })
 })
